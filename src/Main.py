a8ffbbbf0946fbfcb32721c9c50b21a932acbb52
Per Andersson <avtobiff@gmail.com>
2009-05-04 23:04:50 +0100

    First steps toward main loop.

---
+++ src/Main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+
+
+pygame.init()
+screen = pygame.display.set_mode((640,480))
+pygame.display.set_caption("Super Mario Långben Bruce Lee 2")
+pygame.mouse.set_visible(0)
+
+
+canvas = pygame.Surface(screen.get_size())
+
+
+while 1:
+    pass
