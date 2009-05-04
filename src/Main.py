41b9f71ced222599a19cb9542ce32858fcc8c80a
Per Andersson <avtobiff@gmail.com>
2009-05-04 23:31:04 +0100

    added psychedelic effect to main loop for good show.

--- src/Main.py
+++ src/Main.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import random
 import pygame
 from pygame.locals import *
 
 
-pygame.init()
-screen = pygame.display.set_mode((640,480))
-pygame.display.set_caption("Super Mario Långben Bruce Lee 2")
-pygame.mouse.set_visible(0)
+class SMLBL2(object):
+    def __init__(self):
+        self.running = 1
+
+    def input(self,event):
+        if event.type == pygame.QUIT:
+            self.running = 0
+
+    def init(self):
+        pygame.init()
+        screen = pygame.display.set_mode((640,480))
+        pygame.display.set_caption("Super Mario Långben Bruce Lee 2")
+        pygame.mouse.set_visible(0)
+
+        while self.running:
+            self.input(pygame.event.poll())
+            screen.fill((random.random()*255,random.random()*255,random.random()*255))
+            pygame.display.flip()
 
 
-canvas = pygame.Surface(screen.get_size())
-
-
-while 1:
-    pass
+if __name__ == "__main__":
+    smlbl2 = SMLBL2()
+    smlbl2.init()
