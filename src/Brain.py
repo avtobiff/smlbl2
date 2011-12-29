fc153a1972259e1df8ae0592e7e0a6dd65e1153b
Per Andersson <avtobiff@gmail.com>
2011-12-29 23:45:11 +0100

    Proper MVC and extended brain function.

--- src/Brain.py
+++ src/Brain.py
@@ -1,6 +1,59 @@
+from models import SplashImage, LevelSplashImage, Level
+from models.CharacterSelect import Background, Character, Cursor
+
+
 class Brain(object):
     """
     Model class.
     """
-    def __init__(self,eye):
-        self.eye = eye
+
+    # game states
+    SPLASH           = 0
+    CHARACTER_SELECT = 1
+    LEVEL_SPLASH     = 2
+    GAMEPLAY         = 3
+
+
+    def __init__(self):
+        self.scene = {self.SPLASH:           self.splash,
+                      self.CHARACTER_SELECT: self.character_select,
+                      self.LEVEL_SPLASH:     self.level_intro,
+                      self.GAMEPLAY:         self.gameplay}
+
+        self.next_state = self.SPLASH
+
+
+
+    def tick(self):
+        """
+        Tick function for game play. This updates model with respect to
+        control from user.
+        """
+        self.scene[self.next_state]()
+
+    ## game states
+    # the idea is to control and repaint everything from the beginning of the
+    # list first and continue backwards
+    def splash(self):
+        self.next_state = self.CHARACTER_SELECT
+        self.current_scene = [SplashImage()]
+
+
+    def character_select(self):
+        self.next_state = self.LEVEL_SPLASH
+        self.current_scene = [Background(),
+                              Character(100, 300, "data/character_0.png", 0),
+                              Character(200, 300, "data/character_1.png", 1),
+                              Character(300, 300, "data/character_2.png", 2),
+                              Character(400, 300, "data/character_3.png", 3),
+                              Cursor(100, 300, "data/cursor.png",
+                                     [100, 200, 300, 400])]
+
+
+    def level_intro(self):
+        self.next_state = self.GAMEPLAY
+        self.current_scene = [LevelSplashImage()]
+
+
+    def gameplay(self):
+        self.current_scene = [Level()]
--- src/Eye.py
+++ src/Eye.py
@@ -8,7 +8,8 @@ class Eye(object):
     """
     Viewer class.
     """
-    def __init__(self):
+    def __init__(self, brain):
+        self.brain = brain
         self.screen = pygame.display.set_mode((640,480))
         pygame.display.set_caption("Super Mario Långben Bruce Lee 2")
         pygame.mouse.set_visible(0)
